925f99519894619dd7547f8e5ba1068a5755c6ca
Samyak Jain <imsamyak18@gmail.com>
2026-09-01 23:47:56 +0530

    feat: add database configuration module for DynamoDB environment variables

--- services/server/src/config/db.config.ts
+++ services/server/src/config/db.config.ts
@@ -2,7 +2,7 @@ import LoadEnv from "@app/utils/LoadEnv";
 import { z } from "zod";
 
 const endpoint = LoadEnv.of("DYNAMODB_ENDPOINT", z.string().optional()).get();
-const region = LoadEnv.of("AWS_REGION", z.string().default("us-east-1")).get();
+const region = LoadEnv.of("AWS_REGION", z.string().default("us-west-2")).get();
 const tableName = LoadEnv.of(
   "DYNAMODB_TABLE_NAME",
   z.string().default("dev-urlshortner-data-table"),
