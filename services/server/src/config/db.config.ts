ff078e7447615f3b0934696a6e0220a3f9872ee0
Samyak Jain <imsamyak18@gmail.com>
2026-09-01 23:43:51 +0530

    feat: initialize DynamoDB configuration with environment variables and zod validation

--- services/server/src/config/db.config.ts
+++ services/server/src/config/db.config.ts
@@ -5,7 +5,7 @@ const endpoint = LoadEnv.of("DYNAMODB_ENDPOINT", z.string().optional()).get();
 const region = LoadEnv.of("AWS_REGION", z.string().default("us-east-1")).get();
 const tableName = LoadEnv.of(
   "DYNAMODB_TABLE_NAME",
-  z.string().default("URL_SHORTNER_TABLE"),
+  z.string().default("url_shortner_table"),
 ).get();
 
 const dbConfig = {
